aa927c62f126ca4f6f1ea7207009a510a4435075
sumit007-ui <sk78146083@gmail.com>
2024-10-24 09:32:09 +0000

    added template and static folder

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def contact_us():
+    return "<p>Hello, World!</p>"
+
+@app.route("/")
+def login_page():
+    return "<p>Hello, World!</p>"
+    
+@app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
